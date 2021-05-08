0e5e4ebc10ff2c0e9161be585e5089e00311264d
FengShun <fengshun95@gmail.com>
2021-05-08 08:37:15 +0800

    Add multi-publisher and multi-subscriber test cases

--- tests/pub_sub_test.cc
+++ tests/pub_sub_test.cc
@@ -4,23 +4,21 @@
 #include <random>
 #include <thread>
 
-#include "include/var_pub_sub/data_node.h"
+#include "../include/var_pub_sub/data_node.h"
 
 TEST(pub_sub, multi_pub_sub) {
   auto data_node = var_pub_sub::DataNode::CreateDataNode(900 * 1024);
 
   std::vector<int32_t> vec;
   vec.resize(20 * 1024);
-  for (auto i = 0; i < vec.size(); i++) {
-    vec[i] = i;
-  }
+  for (auto i = 0; i < vec.size(); i++) vec[i] = i;
 
   auto publish_thread = [&] {
     auto pub = data_node->CreatePublisher();
-    std::default_random_engine random_engine(time(nullptr));
+    std::default_random_engine random_engine(pthread_self());
     std::uniform_int_distribution<size_t> distribution(1, vec.size());
 
-    int times = 20 * 30;
+    int times = 20;
     while (times--) {
       pub.WriteDataPacket(vec.data(),
                           distribution(random_engine) * sizeof(vec[0]));
@@ -28,42 +26,32 @@ TEST(pub_sub, multi_pub_sub) {
     }
   };
 
-  std::thread pub_thread{publish_thread};
-
-  //  std::vector<std::thread> thread_vector;
-  //  thread_vector.reserve(10);
-  //  for (int i = 0; i < 1; i++) {
-  //    thread_vector.emplace_back(publish_thread);
-  //  }
-  //
-  //  for (auto &i : thread_vector) {
-  //    i.join();
-  //  }
+  std::vector<std::thread> pub_threads;
+  pub_threads.reserve(10);
+  for (int i = 0; i < pub_threads.capacity(); i++) {
+    pub_threads.emplace_back(publish_thread);
+  }
 
   auto subscriber_thread = [&] {
     auto sub = data_node->CreateSubscriber();
-    std::default_random_engine random_engine(time(nullptr));
-    std::uniform_int_distribution<size_t> distribution(1, vec.size());
-
     while (sub.ReadWaitIfEmpty(100)) {
       const auto &data = sub.get_data();
-      std::cout << "data->size:" << data.size() << std::endl;
 
       std::vector<int32_t> vec_copy;
-      vec_copy.resize(data.size() / 4);
+      vec_copy.resize(data.size() / sizeof(int32_t));
       memcpy(vec_copy.data(), data.data(), data.size());
 
       for (auto i = 0; i < vec_copy.size(); i++) {
-        if (vec[i] != vec_copy[i]) {
-          std::cout << "Bug" << std::endl;
-        }
         ASSERT_EQ(vec[i], vec_copy[i]) << "i: " << i;
       }
     }
-    std::cout << "Over" << std::endl;
   };
-  std::thread sub_thread{subscriber_thread};
+  std::vector<std::thread> sub_threads;
+  sub_threads.reserve(10);
+  for (int i = 0; i < sub_threads.capacity(); i++) {
+    sub_threads.emplace_back(subscriber_thread);
+  }
 
-  pub_thread.join();
-  sub_thread.join();
+  for (auto &i : pub_threads) i.join();
+  for (auto &i : sub_threads) i.join();
 }
