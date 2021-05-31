c0da7a8970be5c928604fa3dd991bae0c3385ec8
FengShun <shawnfeng0@gmail.com>
2021-05-31 15:54:36 +0800

    Add statistics on the amount of data published and subscribed

--- tests/pub_sub_test.cc
+++ tests/pub_sub_test.cc
@@ -12,6 +12,7 @@ TEST(pub_sub, cpp_multi_pub_sub) {
   vec.resize(20 * 1024);
   for (auto i = 0; i < vec.size(); i++) vec[i] = i;
 
+  std::atomic_uint64_t pub_counter{0};
   auto publish_thread = [&] {
     auto pub = data_node.CreatePublisher();
     std::default_random_engine random_engine(pthread_self());
@@ -19,8 +20,9 @@ TEST(pub_sub, cpp_multi_pub_sub) {
 
     int times = 20;
     while (times--) {
-      pub->WriteDataPacket(vec.data(),
-                           distribution(random_engine) * sizeof(vec[0]));
+      auto length = distribution(random_engine) * sizeof(vec[0]);
+      pub->WriteDataPacket(vec.data(), length);
+      pub_counter += length;
       usleep(30 * 1000);
     }
     vrb::DataNode::DestroyPublisher(pub);
@@ -34,8 +36,10 @@ TEST(pub_sub, cpp_multi_pub_sub) {
 
   auto subscriber_thread = [&] {
     auto sub = data_node.CreateSubscriber();
+    uint64_t sub_counter = 0;
     while (sub->ReadWaitIfEmpty(100)) {
       const auto &data = sub->get_data();
+      sub_counter += data.size();
 
       std::vector<int32_t> vec_copy;
       vec_copy.resize(data.size() / sizeof(int32_t));
@@ -45,8 +49,11 @@ TEST(pub_sub, cpp_multi_pub_sub) {
         ASSERT_EQ(vec[i], vec_copy[i]) << "i: " << i;
       }
     }
+    std::cout << "Sub count: " << sub_counter << std::endl;
+
     vrb::DataNode::DestroySubscriber(sub);
   };
+
   std::vector<std::thread> sub_threads;
   sub_threads.reserve(10);
   for (int i = 0; i < sub_threads.capacity(); i++) {
@@ -55,5 +62,5 @@ TEST(pub_sub, cpp_multi_pub_sub) {
 
   for (auto &i : pub_threads) i.join();
   for (auto &i : sub_threads) i.join();
+  std::cout << "Pub count: " << pub_counter << std::endl;
 }
-
